d9d3654bbae016aad5733504111434c8779b5034
Shruti <shrutigawande1910@gmail.com>
2023-09-15 19:29:23 +0530

    refactor : removed unused imports

--- src/pages/EventDetail.tsx
+++ src/pages/EventDetail.tsx
@@ -1,11 +1,5 @@
-import React from 'react'
-
 function EventDetail() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
 
-export default EventDetail
+export default EventDetail;

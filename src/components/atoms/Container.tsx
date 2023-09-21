d8b6c90935325b69908c0f69151238ea42b6e465
Shruti <shrutigawande1910@gmail.com>
2023-09-21 16:36:58 +0530

    component: Container

---
+++ src/components/atoms/Container.tsx
@@ -0,0 +1,13 @@
+type Props={
+    children : string | JSX.Element | JSX.Element[]
+}
+
+const Container=({children}: Props)=> {
+  return (
+    <div className = 'mx-auto max-w-7xl sm:px-6 lg:px-8'>
+      {children}
+    </div>
+  )
+}
+
+export default Container
